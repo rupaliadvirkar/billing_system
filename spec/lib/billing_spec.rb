f6a3ad8daef00cd6179bccdc00f534670f514108
Rupali Advirkar <rupalikatdare@gmail.com>
2012-11-21 19:51:28 +0530

    boundary conditions added in rspec and refactor

--- spec/lib/billing_spec.rb
+++ spec/lib/billing_spec.rb
@@ -5,7 +5,7 @@ describe Billing do
   end
   
   before(:all) do
-	@dummy = DummyClass.new
+	  @dummy = DummyClass.new
     @dummy.extend Billing
     @items  = []
     @items << Item.new('G',50.50)
@@ -28,22 +28,22 @@ describe Billing do
   	@dummy.amount_eligible_for_percentage_discount(@items).should == 720
   end
   describe "calculation of discount based on predefined percentage on the basis of type of user" do
-  	context "user is emplyoee" do
+  	context "when user is emplyoee" do
   	  it "should retrun discount which is 30% of the eligible amount" do
   	  	@dummy.discount_based_on_percentage(30,720).should == 216
   	  end	
     end
-    context "user is affiliate" do     	
+    context "when user is affiliate" do     	
       it "should retrun discount which is 10% of the eligible amount" do
       	@dummy.discount_based_on_percentage(10,720).should == 72
   	  end	
     end
-    context "user is customer over 2 yrs" do
+    context "when user is customer over 2 yrs" do
       it "should retrun discount which is 5% of the eligible amount" do
       	@dummy.discount_based_on_percentage(5,720).should == 36
   	  end	
     end
-    context "user is customer for less than 2 yrs" do
+    context "when user is customer for less than 2 yrs" do
       it "should return discount as zero" do
       	@dummy.discount_based_on_percentage(0,720).should == 0
       end	
@@ -51,25 +51,25 @@ describe Billing do
   end
 
   describe "calculation of net payable amount for a user" do
-  	context "user is emplyoee" do
+  	context "when user is emplyoee" do
   	  it "should retrun net payable amount" do
   	  	@dummy.calculate_net_amount_payable(30,@items).should == 1334.5
   	  end	
     end
-    context "user is affiliate" do     	
+    context "when user is affiliate" do     	
       it "should retrun net payable amount" do
       	@dummy.calculate_net_amount_payable(10,@items).should == 1473.5
   	  end	
     end
-    context "user is customer over 2 yrs" do
+    context "when user is customer over 2 yrs" do
       it "should retrun net payable amount" do
       	@dummy.calculate_net_amount_payable(5,@items).should == 1509.5
   	  end	
     end
-    context "user is customer for less than 2 yrs" do
+    context "when user is customer for less than 2 yrs" do
       it "should retrun net payable amount" do
       	@dummy.calculate_net_amount_payable(0,@items).should == 1540.5
       end	
     end  	 	
-  end
+  end  
 end
